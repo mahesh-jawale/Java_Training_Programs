b2a7cb5b2b3da7121840653e5fd7d7406da17535
mahesh-jawale <71960310+mahesh-jawale@users.noreply.github.com>
2021-11-17 17:30:49 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,27 @@
+package com.variables;
+
+public class Variables
+{
+	int j;//Instance Variable
+	public static int k;//Static Variable
+	
+	public static void main(String[] args)
+	{
+		int i=10;//Local Variable
+		Variables obj=new Variables();
+		Variables obj1=new Variables();
+		obj.j=20;
+		obj1.j=40;
+		k=60;
+		System.out.println("Local Variable i="+i);
+		System.out.println("Instance variable j="+obj.j);
+		System.out.println("Instance variable j="+obj1.j);
+		System.out.println("Static variable k="+k);
+		
+	
+		
+		
+
+	}
+
+}
